87452a6fa7558dd6ba3da79fe37ab16f0337cd1a
Alvaro Cano <alvaro.cano@zevenet.com>
2019-12-23 18:17:47 +0100

    [Improve] Create a tiny set to debug tasks
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   src/ZCLI/Objects.pm
    	new file:   src/ZCLI/Objects/Debug.pm

--- src/ZCLI/Objects.pm
+++ src/ZCLI/Objects.pm
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Hash::Merge;
+use ZCLI::Define;
 
 package Objects;
 
@@ -15,65 +16,24 @@ use ZCLI::Objects::Ipds;
 use ZCLI::Objects::System;
 use ZCLI::Objects::Rbac;
 
-#~ our $Zcli = {};
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Farms );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Interfaces );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Certificates );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Farmguardian );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Statistics );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Ipds );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::System );
-#~ $Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Rbac );
+our $Zcli = {};
+
+if ( $Global::DEBUG > 1 )
+{
+	require ZCLI::Objects::Debug;
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Debug );
+}
+else
+{
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Farms );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Interfaces );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Certificates );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Farmguardian );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Statistics );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Ipds );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::System );
+	$Objects::Zcli = &Hash::Merge::merge( $Objects::Zcli, $Objects::Rbac );
+}
 
 1;
 
-#### DEBUG!!
-use ZCLI::Define;
-
-# verbs
-my %V = %Define::Actions;
-
-# keys
-my %K = %Define::Keys;
-my $debug = {
-	'farms-services' => {
-						  $V{ CREATE } => {
-											uri    => "/farms/<$K{FARM}>/services",
-											method => 'POST',
-						  },
-						  $V{ SET } => {
-										 uri    => "/farms/<$K{FARM}>/services/<$K{SRV}>",
-										 method => 'PUT',
-						  },
-	},
-	'farms' => {
-				 $V{ STOP } => {
-								 uri    => "/farms/<$K{FARM}>/actions",
-								 method => 'PUT',
-								 params => {
-											 'action' => 'stop',
-								 },
-				 },
-	},
-
-	'system-backups' => {
-						  $V{ DOWNLOAD } => {
-											  uri    => "/system/backup/<$K{BACKUP}>",
-											  method => 'GET',
-											  download_file => undef,
-						  },
-						  $V{ UPLOAD } => {
-								   uri          => "/system/backup/$Define::UriParamTag",
-								   method       => 'PUT',
-								   content_type => 'application/gzip',
-								   upload_file  => undef,
-								   uri_param    => [
-										   {
-											 name => "name",
-											 desc => "the name which the backup will be saved",
-										   },
-								   ],
-						  },
-	}
-};
-our $Zcli = $debug;
---
+++ src/ZCLI/Objects/Debug.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use ZCLI::Define;
+
+# verbs
+my %V = %Define::Actions;
+
+# keys
+my %K = %Define::Keys;
+
+package Objects;
+
+our $Debug = {
+	'farms-services' => {
+						  $V{ CREATE } => {
+											uri    => "/farms/<$K{FARM}>/services",
+											method => 'POST',
+						  },
+						  $V{ SET } => {
+										 uri    => "/farms/<$K{FARM}>/services/<$K{SRV}>",
+										 method => 'PUT',
+						  },
+	},
+	'farms' => {
+				 $V{ STOP } => {
+								 uri    => "/farms/<$K{FARM}>/actions",
+								 method => 'PUT',
+								 params => {
+											 'action' => 'stop',
+								 },
+				 },
+	},
+
+	'system-backups' => {
+						  $V{ DOWNLOAD } => {
+											  uri    => "/system/backup/<$K{BACKUP}>",
+											  method => 'GET',
+											  download_file => undef,
+						  },
+						  $V{ UPLOAD } => {
+								   uri          => "/system/backup/$Define::UriParamTag",
+								   method       => 'PUT',
+								   content_type => 'application/gzip',
+								   upload_file  => undef,
+								   uri_param    => [
+										   {
+											 name => "name",
+											 desc => "the name which the backup will be saved",
+										   },
+								   ],
+						  },
+	}
+};
+
+1;
