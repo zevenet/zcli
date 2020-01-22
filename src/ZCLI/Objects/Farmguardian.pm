c503c3c2dfb8aa4e8d02b8daca5937aa62184ef0
Alvaro Cano <alvaro.cano@zevenet.com>
2020-01-22 16:32:38 +0100

    [Improvement] Add Affero license to the proyect
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- src/ZCLI/Objects/Farmguardian.pm
+++ src/ZCLI/Objects/Farmguardian.pm
@@ -1,4 +1,25 @@
 #!/usr/bin/perl
+###############################################################################
+#
+#    Zevenet Software License
+#    This file is part of the Zevenet Load Balancer software package.
+#
+#    Copyright (C) 2014-today ZEVENET SL, Sevilla (Spain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 use strict;
 use warnings;
@@ -14,28 +35,28 @@ my %K = %Define::Keys;
 package Objects;
 
 our $Farmguardian = {
-	'farmguardian' => {
-		$V{ LIST } => {
-						uri    => "/monitoring/fg",
-						method => 'GET',
-		},
-		$V{ GET } => {
-					   uri    => "/monitoring/fg/<$K{FG}>",
-					   method => 'GET',
-		},
-		$V{ CREATE } => {
-						  uri    => "/monitoring/fg",
-						  method => 'POST',
-		},
-		$V{ SET } => {
-					   uri    => "/monitoring/fg/<$K{FG}>",
-					   method => 'PUT',
-		},
-		$V{ DELETE } => {
-						  uri    => "/monitoring/fg/<$K{FG}>",
-						  method => 'DELETE',
-		},
-	},
+					  'farmguardian' => {
+										  $V{ LIST } => {
+														  uri    => "/monitoring/fg",
+														  method => 'GET',
+										  },
+										  $V{ GET } => {
+														 uri => "/monitoring/fg/<$K{FG}>",
+														 method => 'GET',
+										  },
+										  $V{ CREATE } => {
+															uri    => "/monitoring/fg",
+															method => 'POST',
+										  },
+										  $V{ SET } => {
+														 uri => "/monitoring/fg/<$K{FG}>",
+														 method => 'PUT',
+										  },
+										  $V{ DELETE } => {
+														   uri => "/monitoring/fg/<$K{FG}>",
+														   method => 'DELETE',
+										  },
+					  },
 };
 
 1;
--- src/ZCLI/Objects/Statistics.pm
+++ src/ZCLI/Objects/Statistics.pm
@@ -1,4 +1,25 @@
 #!/usr/bin/perl
+###############################################################################
+#
+#    Zevenet Software License
+#    This file is part of the Zevenet Load Balancer software package.
+#
+#    Copyright (C) 2014-today ZEVENET SL, Sevilla (Spain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 use strict;
 use warnings;
@@ -27,10 +48,10 @@ our $Statistics = {
 										  },
 					},
 					'statistics-network-connections' => {
-										  $V{ GET } => {
-												  uri => "/stats/system/connections",
-												  method => 'GET',
-										  },
+												 $V{ GET } => {
+														 uri => "/stats/system/connections",
+														 method => 'GET',
+												 },
 					},
 					'statistics-farms' => {
 											$V{ LIST } => {
@@ -38,8 +59,8 @@ our $Statistics = {
 															method => 'GET',
 											},
 											$V{ GET } => {
-													  uri => "/stats/farms/<$K{FARM}>",
-													  method => 'GET',
+														   uri => "/stats/farms/<$K{FARM}>",
+														   method => 'GET',
 											},
 					},
 };
