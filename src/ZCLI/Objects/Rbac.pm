c033a24c89e4c3295e283a36a79b644825ce28a9
Alvaro Cano <alvaro.cano@zevenet.com>
2020-04-08 18:20:15 +0200

    [Improvement] Add commands to manage the LDAP configuration
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modificado:     src/ZCLI/Objects/Rbac.pm

--- src/ZCLI/Objects/Rbac.pm
+++ src/ZCLI/Objects/Rbac.pm
@@ -35,64 +35,64 @@ my %K = %Define::Keys;
 package Objects;
 
 our $Rbac = {
-	'rbac-user' => {
-					 $V{ LIST } => {
-									 uri        => "/rbac/users",
-									 method     => 'GET',
-									 enterprise => 1,
-					 },
-					 $V{ GET } => {
-									uri        => "/rbac/users/<$K{RBAC_USER}>",
-									method     => 'GET',
-									enterprise => 1,
-					 },
-					 $V{ CREATE } => {
-									   uri        => "/rbac/users",
-									   method     => 'POST',
-									   enterprise => 1,
-					 },
-					 $V{ SET } => {
-									uri        => "/rbac/users/<$K{RBAC_USER}>",
-									method     => 'PUT',
-									enterprise => 1,
-					 },
-					 $V{ DELETE } => {
-									   uri        => "/rbac/users/<$K{RBAC_USER}>",
-									   method     => 'DELETE',
-									   enterprise => 1,
-					 },
-	},
-	'rbac-group' => {
-					  $V{ LIST } => {
-									  uri        => "/rbac/groups",
-									  method     => 'GET',
-									  enterprise => 1,
-					  },
-					  $V{ GET } => {
-									 uri        => "/rbac/groups/<$K{RBAC_GROUP}>",
-									 method     => 'GET',
-									 enterprise => 1,
-					  },
-					  $V{ CREATE } => {
-										uri        => "/rbac/groups",
-										method     => 'POST',
-										enterprise => 1,
-					  },
-					  $V{ SET } => {
-									 uri                 => "/rbac/groups/<$K{RBAC_GROUP}>",
-									 method              => 'PUT',
-									 params_autocomplete => {
-															  role => ['rbac', 'roles'],
-									 },
-									 enterprise => 1,
-					  },
-					  $V{ DELETE } => {
-										uri        => "/rbac/groups/<$K{RBAC_GROUP}>",
-										method     => 'DELETE',
-										enterprise => 1,
-					  },
-	},
-	'rbac-group-interface' => {
+			  'rbac-user' => {
+							   $V{ LIST } => {
+											   uri        => "/rbac/users",
+											   method     => 'GET',
+											   enterprise => 1,
+							   },
+							   $V{ GET } => {
+											  uri        => "/rbac/users/<$K{RBAC_USER}>",
+											  method     => 'GET',
+											  enterprise => 1,
+							   },
+							   $V{ CREATE } => {
+												 uri        => "/rbac/users",
+												 method     => 'POST',
+												 enterprise => 1,
+							   },
+							   $V{ SET } => {
+											  uri        => "/rbac/users/<$K{RBAC_USER}>",
+											  method     => 'PUT',
+											  enterprise => 1,
+							   },
+							   $V{ DELETE } => {
+												 uri    => "/rbac/users/<$K{RBAC_USER}>",
+												 method => 'DELETE',
+												 enterprise => 1,
+							   },
+			  },
+			  'rbac-group' => {
+								$V{ LIST } => {
+												uri        => "/rbac/groups",
+												method     => 'GET',
+												enterprise => 1,
+								},
+								$V{ GET } => {
+											   uri    => "/rbac/groups/<$K{RBAC_GROUP}>",
+											   method => 'GET',
+											   enterprise => 1,
+								},
+								$V{ CREATE } => {
+												  uri        => "/rbac/groups",
+												  method     => 'POST',
+												  enterprise => 1,
+								},
+								$V{ SET } => {
+											   uri    => "/rbac/groups/<$K{RBAC_GROUP}>",
+											   method => 'PUT',
+											   params_autocomplete => {
+																	  role => ['rbac', 'roles'],
+											   },
+											   enterprise => 1,
+								},
+								$V{ DELETE } => {
+												  uri    => "/rbac/groups/<$K{RBAC_GROUP}>",
+												  method => 'DELETE',
+												  enterprise => 1,
+								},
+			  },
+			  'rbac-group-interface' => {
 					  $V{ ADD } => {
 									 uri    => "/rbac/groups/<$K{RBAC_GROUP}>/interfaces",
 									 method => 'POST',
@@ -106,23 +106,23 @@ our $Rbac = {
 							  method     => 'DELETE',
 							  enterprise => 1,
 					  },
-	},
-	'rbac-group-farm' => {
-						   $V{ ADD } => {
-										  uri    => "/rbac/groups/<$K{RBAC_GROUP}>/farms",
-										  method => 'POST',
-										  params_autocomplete => {
-																   name => ['farms'],
-										  },
-										  enterprise => 1,
-						   },
-						   $V{ REMOVE } => {
+			  },
+			  'rbac-group-farm' => {
+							$V{ ADD } => {
+										   uri    => "/rbac/groups/<$K{RBAC_GROUP}>/farms",
+										   method => 'POST',
+										   params_autocomplete => {
+																	name => ['farms'],
+										   },
+										   enterprise => 1,
+							},
+							$V{ REMOVE } => {
 									uri => "/rbac/groups/<$K{RBAC_GROUP}>/farms/<$K{FARM}>",
 									method     => 'DELETE',
 									enterprise => 1,
-						   },
-	},
-	'rbac-group-user' => {
+							},
+			  },
+			  'rbac-group-user' => {
 					   $V{ ADD } => {
 									  uri    => "/rbac/groups/<$K{RBAC_GROUP}>/users",
 									  method => 'POST',
@@ -136,34 +136,63 @@ our $Rbac = {
 							   method     => 'DELETE',
 							   enterprise => 1,
 					   },
-	},
-	'rbac-role' => {
-		$V{ LIST } => {
-						uri        => "/rbac/roles",
-						method     => 'GET',
-						enterprise => 1,
-		},
-		$V{ GET } => {
-					   uri        => "/rbac/roles/<$K{RBAC_ROLE}>",
-					   method     => 'GET',
-					   enterprise => 1,
-		},
-		$V{ CREATE } => {
-						  uri        => "/rbac/roles",
-						  method     => 'POST',
-						  enterprise => 1,
-		},
-
-		#		$V{ SET } => {
-		#					   uri    => "/rbac/roles/<$K{RBAC_ROLE}>",
-		#					   method => 'PUT',
-		#		},
-		$V{ DELETE } => {
-						  uri        => "/rbac/roles/<$K{RBAC_ROLE}>",
-						  method     => 'DELETE',
-						  enterprise => 1,
-		},
-	},
+			  },
+			  'rbac-role' => {
+							   $V{ LIST } => {
+											   uri        => "/rbac/roles",
+											   method     => 'GET',
+											   enterprise => 1,
+							   },
+							   $V{ GET } => {
+											  uri        => "/rbac/roles/<$K{RBAC_ROLE}>",
+											  method     => 'GET',
+											  enterprise => 1,
+							   },
+							   $V{ CREATE } => {
+												 uri        => "/rbac/roles",
+												 method     => 'POST',
+												 enterprise => 1,
+							   },
+							   $V{ SET } => {
+											  uri    => "/rbac/roles/<$K{RBAC_ROLE}>",
+											  method => 'PUT',
+							   },
+							   $V{ DELETE } => {
+												 uri    => "/rbac/roles/<$K{RBAC_ROLE}>",
+												 method => 'DELETE',
+												 enterprise => 1,
+							   },
+			  },
+			  'rbac-services' => {
+								   $V{ LIST } => {
+												   uri    => "/rbac/services",
+												   method => 'GET',
+								   },
+								   $V{ SET } => {
+												  uri        => "/rbac/services",
+												  method     => 'POST',
+												  enterprise => 1,
+								   },
+			  },
+			  'rbac-services-ldap' => {
+										$V{ GET } => {
+													   uri    => "/rbac/ldap",
+													   method => 'GET',
+										},
+										$V{ SET } => {
+													   uri        => "/rbac/ldap",
+													   method     => 'POST',
+													   enterprise => 1,
+										},
+										$V{ TEST } => {
+														uri        => "/rbac/ldap/actions",
+														method     => 'POST',
+														enterprise => 1,
+														params     => {
+																	'action' => 'test',
+														},
+										},
+			  },
 };
 
 1;
