5ee4667bd3868fd1a2dc28321f859ee59f34f961
Alvaro Cano <alvaro.cano@zevenet.com>
2020-05-20 16:56:32 +0200

    [Bugfix] Error autocompleting alias commands
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modificado:     src/ZCLI/Objects/Alias.pm

--- src/ZCLI/Objects/Alias.pm
+++ src/ZCLI/Objects/Alias.pm
@@ -35,40 +35,52 @@ my %K = %Define::Keys;
 package Objects;
 
 our $Alias = {
-			   'network-alias-backend' => {
-											$V{ LIST } => {
-															uri    => "/aliases/backends",
-															method => 'GET',
-															enterprise => 1,
-											},
-											$V{ SET } => {
-													 uri => "/aliases/backends/<$K{ALIAS}>",
-													 method     => 'PUT',
-													 enterprise => 1,
-											},
-											$V{ DELETE } => {
-													 uri => "/aliases/backends/<$K{ALIAS}>",
-													 method     => 'DELETE',
-													 enterprise => 1,
-											},
-			   },
-			   'network-alias-interface' => {
-										   $V{ LIST } => {
-														   uri    => "/aliases/interfaces",
-														   method => 'GET',
-														   enterprise => 1,
-										   },
-										   $V{ SET } => {
-												   uri => "/aliases/interfaces/<$K{ALIAS}>",
-												   method     => 'PUT',
-												   enterprise => 1,
-										   },
-										   $V{ DELETE } => {
-												   uri => "/aliases/interfaces/<$K{ALIAS}>",
-												   method     => 'DELETE',
-												   enterprise => 1,
-										   },
-			   },
+		  'network-alias-backend' => {
+						 $V{ LIST } => {
+										 uri        => "/aliases/backends",
+										 method     => 'GET',
+										 enterprise => 1,
+						 },
+						 $V{ SET } => {
+								 uri        => "/aliases/backends/$Define::Uri_param_tag",
+								 method     => 'PUT',
+								 enterprise => 1,
+								 param_uri  => [
+											   {
+												 name => "backend IP",
+												 desc => "IP to assign it a friendly name",
+											   },
+								 ],
+						 },
+						 $V{ DELETE } => {
+										   uri        => "/aliases/backends/<$K{ALIAS}>",
+										   method     => 'DELETE',
+										   enterprise => 1,
+						 },
+		  },
+		  'network-alias-interface' => {
+				  $V{ LIST } => {
+								  uri        => "/aliases/interfaces",
+								  method     => 'GET',
+								  enterprise => 1,
+				  },
+				  $V{ SET } => {
+						  uri       => "/aliases/interfaces/$Define::Uri_param_tag",
+						  method    => 'PUT',
+						  param_uri => [
+										{
+										  name => "interface",
+										  desc => "interface to assign it a friendly name",
+										},
+						  ],
+						  enterprise => 1,
+				  },
+				  $V{ DELETE } => {
+									uri        => "/aliases/interfaces/<$K{ALIAS}>",
+									method     => 'DELETE',
+									enterprise => 1,
+				  },
+		  },
 };
 
 1;
