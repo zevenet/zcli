04cd81e48503e1b3b9bfbb9c238edde4a370e74b
Alvaro Arjona <alvaro.arjona@zevenet.com>
2021-07-08 11:34:25 +0200

    [New Feature] Add Aws and Azure support commands
    Signed-off-by: Alvaro Arjona <alvaro.arjona@zevenet.com>
    
    	modified:   src/ZCLI/Objects.pm
    	new file:   src/ZCLI/Objects/Aws.pm
    	new file:   src/ZCLI/Objects/Azure.pm

---
+++ src/ZCLI/Objects/Aws.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+###############################################################################
+#
+#    ZEVENET Software License
+#    This file is part of the ZEVENET Load Balancer software package.
+#
+#    Copyright (C) 2014-today ZEVENET SL, Sevilla (Spain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+use strict;
+use warnings;
+
+use ZCLI::Define;
+
+# verbs
+my %V = %Define::Actions;
+
+# keys
+my %K = %Define::Keys;
+
+package Objects;
+
+our $Aws = {
+		'aws-credentials' => {
+				$V{ GET } => {
+						uri        => "/aws/credentials",
+						method     => 'GET',
+						enterprise => 1,
+				},
+				$V{ SET } => {
+					 	uri        => "/aws/credentials",
+					 	method     => 'POST',
+					 	enterprise => 1,
+				}
+		},
+		'aws-ssh' => {
+				$V{ SET } => {
+						uri        => "/aws/ssh",
+						method     => 'PUT',
+						enterprise => 1,
+				}
+		},
+};
+
+1;
