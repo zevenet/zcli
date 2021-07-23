f6fffb12046b1ad3adb967b8a48c7ed6be093024
Alvaro Cano <alvaro.cano@zevenet.com>
2021-07-23 13:32:03 +0200

    [Improvement] Add dockerfile to build a ZCLI docker image
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
 set -e
+set -x
 
+DOCKER=0
 BIN_PATH="/usr/bin/zcli"
 PERL_PATH="/usr/share/perl5"
 
 OS_FILE="/etc/os-release"
 source $OS_FILE
+
+
+if [[ "$1" != "" ]]; then
+	if [[ "$1" == "-docker" ]]; then
+		DOCKER=1
+		# Var to set autoconfirm option in CPAN
+		PERL_MM_USE_DEFAULT=1
+	elif [[ "$1" =~ "^(-h|--help)$" ]]; then
+		echo "$0 [-docker]"
+		echo "   The docker option accept the interactive dialog in order not to need human interaction"
+	else
+		echo "The option '$1' is not recoignized"
+		exit 1
+	fi
+fi
+
 
 shopt -s nocasematch
 if [[ $NAME =~ "debian" ]]; then
@@ -34,17 +52,26 @@ ZCLI_LIB=$(realpath ${BASE_DIR}/$GIT_ZCLI_LIB)
 DEPENDENCIES="$BASE_DIR/dependencies.txt"
 
 # system packages
+YES=""
+if [[ $DOCKER -eq 1 ]]; then YES="-y"; fi
 if [[ "$OS" == "centos" ]]; then
-	yum install `grep -E "build\b" $DEPENDENCIES | sed -E 's/build\s*=>\s*//'`
-	yum install `grep build_centos $DEPENDENCIES | sed -E 's/build_centos\s*=>\s*//'`
+	yum install `grep -E "build\b" $DEPENDENCIES | sed -E 's/build\s*=>\s*//'` $YES
+	yum install `grep build_centos $DEPENDENCIES | sed -E 's/build_centos\s*=>\s*//'` $YES
 elif [[ "$OS" == "debian" ]]; then
-	apt-get instal `grep -E "build\b" $DEPENDENCIES | sed -E 's/build\s*=>\s*//'`
+	apt-get install $YES `grep -E "build\b" $DEPENDENCIES | sed -E 's/build\s*=>\s*//'`
+	apt-get install $YES `grep -E "build_debian\b" $DEPENDENCIES | sed -E 's/build_debian\s*=>\s*//'`
 else
 	pacman -S `grep -E "build\b" $DEPENDENCIES | sed -E 's/build\s*=>\s*//'`
 fi
 
 # perl modules
 cpan install `grep common $DEPENDENCIES | sed -E 's/common\s*=>\s*//'`
-cpan install `grep linux $DEPENDENCIES | sed -E 's/linux\s*=>\s*//'`
+
+## Necessary, the Term::ReadLine::Gnu module requires human interface for tests
+FORCE=""
+if [[ $DOCKER -eq 1 ]]; then FORCE="-f"; fi
+cpan install $FORCE `grep linux $DEPENDENCIES | sed -E 's/linux\s*=>\s*//'`
 ln -s $ZCLI_BIN $BIN_PATH
 ln -s $ZCLI_LIB $PERL_PATH
+
+exit 0
